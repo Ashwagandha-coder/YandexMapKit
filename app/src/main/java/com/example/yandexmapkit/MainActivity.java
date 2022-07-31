444149a0b084821fc6de37d27010bfc01c7ebf5f
Ashwaganha-cader <max.cacz@yandex.ru>
2022-07-31 12:14:29 +0500

    done base

--- app/src/main/java/com/example/yandexmapkit/MainActivity.java
+++ app/src/main/java/com/example/yandexmapkit/MainActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapView = findViewById(R.id.mapview);
+
+        mapView.getMap().move(
+                new CameraPosition(new Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 0),
+                null);
     }
 }
